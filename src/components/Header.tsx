8b198a2209ffa6d7def91d3b03211e91996f0787
Serrin Doscher <serrin.doscher53@myhunter.cuny.edu>
2023-06-26 18:13:21 -0400

    added header to project with some styling

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+
+const Header = () => {
+  return (
+    <div className="header">
+        <div>
+            <h1>Rescuing Rescues</h1>
+        </div>
+        <div className="header-links">
+            <a>About</a>
+            <a>Data</a>
+            <a>Organizations</a>
+            <a>Links</a>
+        </div>
+    </div>
+  )
+}
+
+export default Header
